e052ea47fad120ffd282cb832f71ab00e251c0be
Phani-Gorantla <phanigorantla531@gmail.com>
2023-05-18 22:44:16 +0530

    User UI created

--- src/App.js
+++ src/App.js
@@ -1,60 +1,80 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { Auth } from './components/auth';
 import { db } from './config/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { useParams } from 'react-router-dom';
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({
+    name: '',
+    mobile: '',
+    website: '',
+    whatsapp: '',
+  });
+
   const { docId } = useParams();
   const docRef = doc(db, 'users', docId);
 
   useEffect(() => {
     const getUser = async () => {
       try {
-        const docSnapshot = await getDoc(docRef);
-        if (docSnapshot.exists()) {
-          const userData = docSnapshot.data();
-          setUser(userData);
-        } else {
-          console.log('Document does not exist');
-        }
-      } catch (error) {
-        console.error('Error fetching user details:', error);
+        const data = await getDoc(docRef);
+        setUser(data.data());
+      } catch (err) {
+        console.error(err);
       }
     };
 
     getUser();
-  }, [docRef]);
+  }, []);
 
   return (
     <div className="App">
-      {/* <Auth /> */}
-      {user && (
-        <div className="profile-container">
-          <div className="profile-header">
-            <img className="profile-image" src={user.profilePic} alt="Profile" />
-            <h2 className="profile-name">{user.name}</h2>
-          </div>
-          <div className="profile-links">
-            <a href={`https://wa.me/${user.whatsapp}`} target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-whatsapp"></i>
-            </a>
-            <a href={user.linkedin} target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href={user.twitter} target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href={user.website} target="_blank" rel="noopener noreferrer">
-              <i className="fas fa-globe"></i>
-            </a>
-          </div>
+      <div className="card-section">
+        <div className="card">
+          {user && (
+            <>
+              <img src={user.profile_picture} alt="Profile" className="profile-picture" />
+              <h2 className="profile-name">{user.name}</h2>
+            </>
+          )}
         </div>
-      )}
+      </div>
+
+      <div className="social-links-container">
+        <ul className="social-links">
+          <li>
+            <a href={user.whatsapp}>
+              <img src="/whatsapp.png" alt="WhatsApp" />
+            </a>
+          </li>
+          <li>
+            <a href={user.linkedin}>
+              <img src="/linkedin.png" alt="LinkedIn" />
+            </a>
+          </li>
+          <li>
+            <a href={user.twitter}>
+              <img src="/twitter.png" alt="Twitter" />
+            </a>
+          </li>
+          <li>
+            <a href={user.website}>
+              <img src="/website.png" alt="Website" />
+            </a>
+          </li>
+          <li>
+            <a href={user.website}>
+              <img src="/phone.png" alt="call" />
+            </a>
+          </li>
+          <li>
+            <a href={user.website}>
+              <img src="/email.png" alt="email" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
