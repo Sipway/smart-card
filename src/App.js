c1bcbbc68f896c9f368a957e68aa56f05b7e9653
AnandKishore2206 <kishore2206@gmail.com>
2023-05-20 16:56:33 +0530

    Calling social Links by looping dynamically

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { db } from './config/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { useParams } from 'react-router-dom';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   const [user, setUser] = useState({
@@ -25,13 +27,29 @@ function App() {
     title: '', // Added "title" field
   });
 
+  const socialLinks = [
+    { id:1, userInfo: user.mobile, medium: 'Mobile', imageSrc: '/phone.png', hypelink: `tel:${user.mobile}` },
+    { id:2, userInfo: user.whatsapp, medium: 'WhatsApp', imageSrc: '/whatsapp.png', hypelink: `https://wa.me/${user.whatsapp}` },
+    { id:3, userInfo: user.facebook, medium: 'Facebook', imageSrc: '/facebook.png', hypelink: `https://www.facebook.com/${user.facebook}`},
+    { id:4, userInfo: user.website, medium: 'Website', imageSrc: '/website.png', hypelink: user.website},
+    { id:5, userInfo: user.instagram, medium: 'Instagram', imageSrc: '/instagram.png', hypelink: `https://www.instagram.com/${user.instagram}`},
+    { id:6, userInfo: user.linkedin, medium: 'LinkedIn', imageSrc: '/linkedin.png', hypelink: `https://www.linkedin.com/in/${user.linkedin}`},
+    { id:7, userInfo: user.twitter, medium: 'Twitter', imageSrc: '/twitter.png', hypelink: `https://www.twitter.com/${user.twitter}`},
+    { id:8, userInfo: user.medium, medium: 'Medium', imageSrc: '/medium.png', hypelink: user.medium},
+    { id:9, userInfo: user.youtube, medium: 'Youtube', imageSrc: '/youtube.png', hypelink: user.youtube},
+    { id:10, userInfo: user.discord, medium: 'Discord', imageSrc: '/discord.png', hypelink: user.discord},
+    { id:11, userInfo: user.onlyfans, medium: 'Onlyfans', imageSrc: '/onlyfans.png', hypelink: user.onlyfans},
+    { id:12, userInfo: user.snapchat, medium: 'Snapchat', imageSrc: '/discord.png', hypelink: `https://www.snapchat.com/add/${user.snapchat}`},
+    { id:13, userInfo: user.location, medium: 'Location', imageSrc: '/location.png', hypelink: user.location},
+    { id:14, userInfo: user.skype, medium: 'Skype', imageSrc: '/skype.png', hypelink: `skype:${user.skype}?chat`},
+  ];
+
   const { docId } = useParams();
-  const docRef = doc(db, 'users', docId);
 
   useEffect(() => {
     const getUser = async () => {
       try {
-        const data = await getDoc(docRef);
+        const data = await getDoc(doc(db, "users", docId));
         setUser(data.data());
       } catch (err) {
         console.error(err);
@@ -60,134 +78,43 @@ END:VCARD`;
   };
 
   return (
-    <div className="App">
-      <div className="card-section">
-        <div className="card">
-          {user && (
-            <>
-              <img src="/profile.png" alt="Profile" className="profile-picture" />
-              <h2 className="profile-name">{user.name}</h2>
-              <p className="profile-title">{user.title}</p>
-              <button className="save-contact-btn" onClick={downloadContact}>
-                Save my contact
-              </button>
-            </>
-          )}
+    <Container>
+      <div className="App">
+        <div className="card-section">
+          <div className="card">
+            {user && (
+              <>
+                <img src="/profile.png" alt="Profile" className="profile-picture" />
+                <h2 className="profile-name">{user.name}</h2>
+                <p className="profile-title">{user.title}</p>
+                <button className="save-contact-btn" onClick={downloadContact}>
+                  Save my contact
+                </button>
+              </>
+            )}
+          </div>
         </div>
-      </div>
 
-      {user && (
-        <div className="social-links-container">
-          <ul className="social-links">
-          {user.mobile && (
-              <li>
-                <a href={`tel:${user.mobile}`}>
-                  <img src="/phone.png" alt="Call" />
-                </a>
-              </li>
-            )}
-            {user.whatsapp && (
-              <li>
-                <a href={`https://wa.me/${user.whatsapp}`}>
-                  <img src="/whatsapp.png" alt="WhatsApp" />
-                </a>
-              </li>
-            )}
-            {user.facebook && (
-              <li>
-                <a href={`https://www.facebook.com/${user.facebook}`}>
-                  <img src="/facebook.png" alt="Facebook" />
-                </a>
-              </li>
-            )}
-            {user.website && (
-              <li>
-                <a href={user.website}>
-                  <img src="/website.png" alt="Website" />
-                </a>
-              </li>
-            )}
-            {user.instagram && (
-              <li>
-                <a href={`https://www.instagram.com/${user.instagram}`}>
-                  <img src="/instagram.png" alt="Instagram" />
-                </a>
-              </li>
-            )}
-            {user.linkedin && (
-              <li>
-                <a href={`https://www.linkedin.com/in/${user.linkedin}`}>
-                  <img src="/linkedin.png" alt="LinkedIn" />
-                </a>
-              </li>
-            )}
-            {user.twitter && (
-              <li>
-                <a href={`https://www.twitter.com/${user.twitter}`}>
-                  <img src="/twitter.png" alt="Twitter" />
-                </a>
-              </li>
-            )}
-            {user.medium && (
-              <li>
-                <a href={user.medium}>
-                  <img src="/medium.png" alt="Medium" />
-                </a>
-              </li>
-            )}
-            {user.youtube && (
-              <li>
-                <a href={user.youtube}>
-                  <img src="/youtube.png" alt="YouTube" />
-                </a>
-              </li>
-            )}
-            {user.discord && (
-              <li>
-                <a href={user.discord}>
-                  <img src="/discord.png" alt="Discord" />
-                </a>
-              </li>
-            )}
-            {user.onlyfans && (
-              <li>
-                <a href={user.onlyfans}>
-                  <img src="/onlyfans.png" alt="OnlyFans" />
-                </a>
-              </li>
-            )}
-            {user.snapchat && (
-              <li>
-                <a href={`https://www.snapchat.com/add/${user.snapchat}`}>
-                  <img src="/snapchat.png" alt="Snapchat" />
-                </a>
-              </li>
-            )}
-            {user.telegram && (
-              <li>
-                <a href={`https://t.me/${user.telegram}`}>
-                  <img src="/telegram.png" alt="Telegram" />
-                </a>
-              </li>
-            )}
-            {user.location && (
-              <li>
-                <a href={user.location}>
-                  <img src="/location.png" alt="Location" />
-                </a>
-              </li>
-            )}
-            {user.skype && (
-              <li>
-                <a href={`skype:${user.skype}?chat`}>
-                  <img src="/skype.png" alt="Skype" />
-                </a>
-              </li>
-            )}
-          </ul>
-        </div>
-      )}
-    </div>
+        {user && (
+          <div className="social-links-container">
+            <ul className="social-links">
+              {socialLinks.map(item => {
+                return (
+                  <div key={item.id}>
+                    {item.userInfo && (
+                      <li>
+                        <a href={item.hypelink}>
+                          <img src={item.imageSrc} alt={item.medium} />
+                        </a>
+                      </li>
+                    )}
+                  </div>);
+              })};
+            </ul>
+          </div>
+        )}
+      </div>
+    </Container>
   );
 }
 
